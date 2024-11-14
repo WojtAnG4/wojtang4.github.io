cfb7761bf4c999ace8d11371d769e025f2dd9080
WojtAnG4 <108181510+WojtAnG4@users.noreply.github.com>
2024-11-14 11:40:31 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function showEquipmentOptions() {
+    // Pokazuje przyciski sprzętu
+    document.getElementById('equipment-options').style.display = 'block';
+}
+
+function showDetails(option) {
+    let detailsText = '';
+    
+    if (option === 'laptop') {
+        detailsText = 'lenovo IdeaPad Slim 3 | AMD Ryzen 5 5500U | 8 GB DDR4 | 512GB SSD | W11';
+    } else if (option === 'pc') {
+        detailsText = 'GA-MA78LM-S2H | AMD Athlona II X2 240 | 3 GB DDR2 | 200GB HDD | W7';
+    }
+
+    // Pokazuje szczegóły sprzętu
+    document.getElementById('equipment-details').style.display = 'block';
+    document.getElementById('details-text').innerText = detailsText;
+}
