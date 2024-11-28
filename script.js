baff01f2c39802f7af61264a5c0d51c02327d2c8
WojtAnG4 <108181510+WojtAnG4@users.noreply.github.com>
2024-11-28 13:51:25 +0100

    jezu
    coraz więcej rzeczy sie dowiaduje o tym kompie za 40zl

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function showDetails(option) {
     if (option === 'laptop') {
         detailsText = 'lenovo IdeaPad Slim 3 | AMD Ryzen 5 5500U | 8 GB DDR4 | 512GB SSD | W11';
     } else if (option === 'pc') {
-        detailsText = 'GA-MA78LM-S2H | AMD Athlon II X2 240 | 3 GB DDR2 | 230GB HDD | W7';
+        detailsText = 'GA-MA78LM-S2H | AMD Athlon II X2 240 | 3 GB DDR2 | 250GB HDD | W7';
     }
 
     document.getElementById('equipment-details').style.display = 'block';
