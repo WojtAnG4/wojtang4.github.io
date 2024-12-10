afc768c45c45e62382b6a28f6033d3223d780122
WojtAnG4 <108181510+WojtAnG4@users.noreply.github.com>
2024-12-10 19:36:34 +0100

    update

--- script.js
+++ script.js
@@ -3,16 +3,30 @@ function showEquipmentOptions() {
 }
 
 function showDetails(option) {
-    let detailsText = '';
+    let detailsHtml = '';
     
     if (option === 'laptop') {
-        detailsText = 'lenovo IdeaPad Slim 3 | AMD Ryzen 5 5500U | 8 GB DDR4 | 512GB SSD | W11';
+        detailsHtml = `
+            <ul>
+                <li><strong>OS:</strong> Windows 11 (Home)</li>
+                <li><strong>CPU:</strong> AMD Ryzen 5 5500U</li>
+                <li><strong>RAM:</strong> 8GB DDR4</li>
+                <li><strong>GPU:</strong> integra</li>
+                <li><strong>HDD:</strong> INTEL SSDPEKNW512GZL</li>
+            </ul>`;
     } else if (option === 'pc') {
-        detailsText = 'GA-MA78LM-S2H | AMD Athlon II X2 240 | 3 GB DDR2 | 250GB HDD | W7';
+        detailsHtml = `
+            <ul>
+                <li><strong>OS:</strong> Windows 7 (Home Premium)</li>
+                <li><strong>CPU:</strong> AMD Athlon II X2 240</li>
+                <li><strong>RAM:</strong> 3GB DDR2</li>
+                <li><strong>GPU:</strong> Geforce GT 240</li>
+                <li><strong>HDD:</strong> SAMSUNG HD251HJ</li>
+            </ul>`;
     }
 
     document.getElementById('equipment-details').style.display = 'block';
-    document.getElementById('details-text').innerText = detailsText;
+    document.getElementById('details-text').innerHTML = detailsHtml;
 }
 
 function showLinks() {
